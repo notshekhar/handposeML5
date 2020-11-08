6bb32e16628f34cc774a62e69108ef03af72f885
Shekhar Tyagi <notshekhar@gmail.com>
2020-11-08 14:30:34 +0530

    added options

--- index.js
+++ index.js
@@ -24,7 +24,14 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         })
 }
 let prediction = []
-let handpose = ml5.handpose(video, {}, modelLoaded)
+let options = {
+    flipHorizontal: false, // boolean value for if the video should be flipped, defaults to false
+    maxContinuousChecks: 10, // How many frames to go without running the bounding box detector. Defaults to infinity, but try a lower value if the detector is consistently producing bad predictions.
+    detectionConfidence: 0.8, // Threshold for discarding a prediction. Defaults to 0.8.
+    scoreThreshold: 0.75, // A threshold for removing multiple (likely duplicate) detections based on a "non-maximum suppression" algorithm. Defaults to 0.75
+    iouThreshold: 0.3, // A float representing the threshold for deciding whether boxes overlap too much in non-maximum suppression. Must be between [0, 1]. Defaults to 0.3.
+}
+let handpose = ml5.handpose(video, options, modelLoaded)
 
 showSpinner("loading model..")
 function modelLoaded() {
@@ -40,10 +47,10 @@ handpose.on("predict", (results) => {
 })
 
 function draw(data) {
-    // ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
     // clean canvas everytime
     ctx.fillStyle = "rgb(33, 33, 33)"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
     for (let hand of data) {
         for (let landmark of hand.landmarks) {
             let [x, y, z] = landmark
@@ -56,5 +63,3 @@ function draw(data) {
     }
     // if (data.length > 0) console.log(data)
 }
-
-
